845d02d9f470d1d25e1d79f7993b0c30ddce172c
BArias10 <83421510+BArias10@users.noreply.github.com>
2021-05-16 11:17:18 -0500

    location functions

--- weather.py
+++ weather.py
@@ -5,23 +5,39 @@
 
 # create main function/welcome message
 
+print("Local Weather Report")
+
 # zip code function
 
 def by_zip():
-        zip_code = int(input('Please Enter Your Zip code: '))
+        zip_code = int(input('Enter Zip code: '))
         url = 'https://api.openweathermap.org/data/2.5/weather?zip={},us&units=imperial&appid=21ad8f1bef4fecfb95bd7d8328a5c54e'.format(zip_code)
         res = requests.get(url)
         data = res.json()
         show_data(data)
 
+        anotherZip = input('Check Weather in a Different Zip Code? Type Yes or No: ')
+        if question == 'Yes':
+        main()
+        if question == 'No':
+        print("Thanks for Using!")
+        exit()
+
 # city search function
 
 def by_city():
-    city = input('Please Enter Your City Name: ')
+    city = input('Enter City Name: ')
     url = 'https://api.openweathermap.org/data/2.5/weather?q={},us&appid=21ad8f1bef4fecfb95bd7d8328a5c54e&units=imperial'.format(city)
     res = requests.get(url)
     data = res.json()
     show_data(data)
+
+    anotherCity = input('Check Weather in a Different City? Type Yes or No: ')
+    if question == 'Yes':
+    main()
+    if question == 'No':
+    print("Thanks for Using!")
+    exit()
 
 # print data/readble format function
 
