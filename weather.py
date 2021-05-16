360c5ec7cadd671dc6a14a5c6ac14b280dbc0b10
BArias10 <83421510+BArias10@users.noreply.github.com>
2021-05-16 11:24:29 -0500

    spacing/format/view functions

--- weather.py
+++ weather.py
@@ -18,10 +18,13 @@ def by_zip():
 
         anotherZip = input('Check Weather in a Different Zip Code? Type Yes or No: ')
         if question == 'Yes':
-        main()
+
+            main()
+
         if question == 'No':
-        print("Thanks for Using!")
-        exit()
+            print("Thanks for Using!")
+
+            exit()
 
 # city search function
 
@@ -41,4 +44,52 @@ def by_city():
 
 # print data/readble format function
 
+def show_data(data):
+    temp = data['main']['temp']
+    hightemp = data['main']['temp_max']
+    lowtemp = data['main']['temp_min']
+    wind_speed = data['wind']['speed']
+    press = data['main']['pressure']
+    latitude = data['coord']['lat']
+    longitude = data['coord']['lon']
+    humid = data['main']['humidity']
+    description = data['weather'][0]['description']
+
+    print('Current Temperature : {} degree fahrenheit'.format(temp))
+    print('High Temperature : {} degree fahrenheit'.format(hightemp))
+    print('Low Temperature : {} degree fahrenheit'.format(lowtemp))
+    print('Wind Speed : {} m/s'.format(wind_speed))
+    print('Pressure : {} hPa'.format(press))
+    print('Latitude : {}'.format(latitude))
+    print('Longitude : {}'.format(longitude))
+    print('Humidity : {} %'.format(humid))
+    print('Description : {}'.format(description))
+
 # ask again/loop
+
+def main():
+    while True:
+        answer = input("Check the Weather! Please type 'zip' for zipcode or 'city' for city name: ")
+        if answer == 'city':
+            try:
+                print("Searching...")
+                by_city()
+
+            except Exception:
+                print("City Not Found")
+                by_city()
+
+        if answer == 'zip':
+            try:
+                print("Searching...")
+                by_zip()
+
+            except Exception:
+                print("Zip Not Found")
+                by_zip()
+
+        else:
+            print("Invalid Entry. Enter one of the options.")
+
+
+main()
